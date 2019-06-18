c23b7267579cf625d4515ade34009982970f6c7a
Yevgen Goliuk <evgeniy.goliuk@sysgears.com>
2019-06-18 20:05:01 +0300

    Add "Registration" page

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@
 # from __future__ import unicode_literals
 from flask import render_template, flash, redirect, request, url_for
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
@@ -44,3 +44,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
