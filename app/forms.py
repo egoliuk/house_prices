bbc8101c2098b3737ab84d8083e2b6ade3a33908
Eugene Goliuk <eugene.goliuk1@davita.com>
2023-08-17 00:06:14 +0300

    Fix import error

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from app.models import User
